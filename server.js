286f59de55c057364320dbe8a968ee8a45ac7d3f
Kosala Yapa Bandara <kosalayb@gmail.com>
2021-03-23 16:47:40 +0000

    dist deleted and server.js updated to connect to metamask

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-  
+
   //direct connection to Ganache/or ethereum
   var provider = 'HTTP://127.0.0.1:7545';
   var web3Provider = new Web3.providers.HttpProvider(provider);
   var web3 = new Web3(web3Provider);
+
 
   var contractAddress ='0x1d1d6FcbCd2DcEC79804BD0e91e68f0093C1cDa8';
   var contractABI;
@@ -29,23 +30,30 @@
   var instance = new web3.eth.Contract(contractABI, contractAddress);
   var account;
 
-
-
-  //finding accounts
- web3.eth.getAccounts(function(err, accounts) {
-
-    if (err != null) {
-      alert("Error retrieving accounts.");
-      return;
-    }
-    if (accounts.length == 0) {
-      alert("No account found! Make sure the Ethereum client is configured properly.");
-      return;
-    }
-    account = accounts[1];
-    console.log('Account: ' + account);
+  //connect to accounts selected from metamask client
+  ethereum.enable();
+  ethereum.on('accountsChanged', function (accounts) {
+    account = accounts[0];
     web3.eth.defaultAccount = account;
+    console.log("Current Acoount :" + account);
   });
 
+   // //finding accounts
+   // web3.eth.getAccounts(function(err, accounts) {
+
+   //    if (err != null) {
+   //      alert("Error retrieving accounts.");
+   //      return;
+   //    }
+   //    if (accounts.length == 0) {
+   //      alert("No account found! Make sure the Ethereum client is configured properly.");
+   //      return;
+   //    }
+
+   //    account = accounts[1];
+   //    console.log('@server Account is: ' + account);
+   //    web3.eth.defaultAccount = account;
+
+   //  });
 
 
